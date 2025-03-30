75ade2e8c734bb4d7f235630c0a90a12c5c87a5f
TysonPellatt <59511115+TysonPellatt@users.noreply.github.com>
2025-03-30 18:26:58 -0400

    Update RAM-Test.py to try and fix exception error. no has an erro with I/O due to list needing int or slice instead of str (starts with total_mem

--- RAM-Test.py
+++ RAM-Test.py
@@ -20,5 +20,23 @@ def ram_usage():
         print(f"Free Memory:\n{free_mem / 1024: .2f} MB")
     except Exception as e:
         print("An error occured: {e}")
-    
-ram_usage()
+
+#testing
+file = open("/proc/meminfo", "r")
+lines = file.readlines()
+
+mem_info = {}
+for lines in file:
+    line_parts = lines.split(':')
+    key = line_parts[0].strip()
+    value = line_parts[1].strip()
+    mem_info[key] = int(value.split()[0])
+
+total_mem = lines['MemTotal', 0]
+free_mem = lines['MemFree', 0]
+used_mem = total_mem - free_mem
+
+print(f"Used Memory:\n{used_mem / 1024: .2f} MB")
+print(f"Free Memory:\n{free_mem / 1024: .2f} MB") 
+file.close()  
+#ram_usage()
