0feb07fad035bcd83fdf29156bb5cc0e72827269
BobChilling <caoways@gmail.com>
2025-04-02 13:30:28 -0400

    Updated draft

--- CPU Usage.py
+++ CPU Usage.py
@@ -8,14 +8,18 @@ def cpu_usage():
     try:
         cpu_file = open("/proc/stat", "r")
         lines = cpu_file.readlines()
-    
     # Grab the data (user, nice, system, idle) and store it into variables
-
+        for line in lines:
+            if lines.startswith("cpu"):
+                data = line.split()
+                user = data[1]
+                nice = data[2]
+                system = data[3]
+                idle = data [4]
     # Calculate the total and minus idle will estimate the usage
-
-
-
-    
+                total = user+ nice+ system+ idle
+                percent = 100 * (1 - (idle/total))
+                return percent
     except:
         return None
  
