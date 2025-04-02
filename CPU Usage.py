34a53a1113c6f22d8a60f2759c4e35afdac85c1f
BobChilling <caoways@gmail.com>
2025-04-02 13:16:32 -0400

    Researching key concepts to achieve the goal

--- CPU Usage.py
+++ CPU Usage.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 # Author: Wcao23
 
-'''
-Getting a single snapshot of CPU usage
-'''
+
+def cpu_usage():
+    '''Getting a single snapshot of cpu usage'''
+    # Access the CPU usage file and read the data
+    try:
+        cpu_file = open("/proc/stat", "r")
+        lines = cpu_file.readlines()
+    
+    # Grab the data (user, nice, system, idle) and store it into variables
+
+    # Calculate the total and minus idle will estimate the usage
+
+
+
+    
+    except:
+        return None
+ 
+
+
+
+
+
+
